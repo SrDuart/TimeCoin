0f35a979daa7b8b26736ea2b11d4275e95ef759d
Petrus de Melo <petrusdemelo@gmail.com>
2017-11-07 20:52:25 -0300

    Criado o AnuncioSqlServer

---
+++ TimeCoin/Biblioteca/Parametros/AnuncioSqlServer.cs
@@ -0,0 +1,39 @@
+﻿using Biblioteca.ConexaoBanco;
+using Biblioteca.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Biblioteca.ClassesBasicas;
+
+namespace Biblioteca.Parametros
+{
+    class AnuncioSqlServer : ConexaoSqlServer, IAnuncio
+    {
+        public void Delete(Anuncio anuncio)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Anuncio anuncio)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Anuncio> Select(Anuncio filtro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Anuncio anuncio)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool VerificaDuplicidade(Anuncio anuncio)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
