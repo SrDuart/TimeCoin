2d064fc8f7238bde0f463e8944607af83a934b50
Petrus de Melo <petrusdemelo@gmail.com>
2017-11-11 00:24:35 -0300

    Criado a interface ITipoAnuncio

---
+++ TimeCoin/Biblioteca/Interfaces/ITipoAnuncio.cs
@@ -0,0 +1,18 @@
+﻿using Biblioteca.ClassesBasicas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Interfaces
+{
+    public interface ITipoAnuncio
+    {
+        void Insert(TipoAnuncio tipoAnuncio);
+        void Update(TipoAnuncio tipoAnuncio);
+        void Delete(TipoAnuncio tipoAnuncio);
+        bool VerificaDuplicidade(TipoAnuncio tipoAnuncio);
+        List<TipoAnuncio> Select(TipoAnuncio filtro);
+    }
+}
