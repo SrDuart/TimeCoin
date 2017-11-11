14cd7a1efd2b634809375c1ca62d5332cac901f7
Petrus de Melo <petrusdemelo@gmail.com>
2017-11-11 01:01:50 -0300

    alterado o nome do atributo date para data

--- TimeCoin/Biblioteca/ClassesBasicas/Anuncio.cs
+++ TimeCoin/Biblioteca/ClassesBasicas/Anuncio.cs
@@ -9,7 +9,7 @@ namespace Biblioteca.ClassesBasicas
         public Situacao situacao { get; set; }
         public TipoAnuncio tipoAnuncio { get; set; }
 		public string descricao { get; set; }
-		public DateTime date { get; set; }		
+		public DateTime data { get; set; }		
 		public string preRequisito { get; set; }
         public string uf { get; set; }
         public string bairro { get; set; }
