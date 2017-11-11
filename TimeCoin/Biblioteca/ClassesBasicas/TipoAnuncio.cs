51dde71aad4f103d5b5844820db174663b378fa4
Petrus de Melo <petrusdemelo@gmail.com>
2017-11-11 00:15:40 -0300

    Alterado a visibilidade de TipoAnuncio

--- TimeCoin/Biblioteca/ClassesBasicas/TipoAnuncio.cs
+++ TimeCoin/Biblioteca/ClassesBasicas/TipoAnuncio.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Biblioteca.ClassesBasicas
 {
-    class TipoAnuncio
+    public class TipoAnuncio
     {
         public int id { get; set; }
         public string descricao { get; set; }
